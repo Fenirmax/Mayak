6fec6426698ead3a780546ba0709c4e2fa86146f
Fenirmax <121963998+Fenirmax@users.noreply.github.com>
2023-01-20 18:48:11 +0200

    Update Genuch.ino

--- Genuch.ino
+++ Genuch.ino
@@ -5,7 +5,8 @@
 
 RF24 radio(9, 8);          // CE, CSN
 const byte address[10] = "ADDRESS01";
-const genon[] = "0", mayakon[] = "1"
+const char genon[] = "0";
+const char mayakon[] = "1";
 
 void setup() {
 Serial.begin(9600);
@@ -17,5 +18,5 @@ radio.begin();
 
 void loop() {
 radio.write(&genon, sizeof(genon)); Serial.println(genon);
-delay(1000)
+delay(1000) ;
 }
